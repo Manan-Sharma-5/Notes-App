731231a0c81b2bdf251fcc885b195c0c08743388
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2023-06-06 01:38:00 +0530

    added server files

---
+++ server/server.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const router = express.Router();
+
+app.use(cors());
+
+app.use(express.json()); 
+
+app.listen(8000, console.log('listening on port 8000'));
+
+const mongoURI = 'mongodb+srv://mashmanan:258dxU31Jkxg62vw@cluster0.hgatok1.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+  const noteSchema = new mongoose.Schema({
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    title: String,
+    content: String,
+  });
+
+const Note = mongoose.model('Note', noteSchema);
+
+const userScheme = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userScheme);
+
+app.post('/notes', async (req, res) => {
+  const { authorization } = req.headers;
+  const [, token] = authorization.split(' ');
+  const [username, password] = token.split(':');
+  const user = await User.findOne({ username }).exec();
+  if (!user || user.password !== password) {
+    res.status(403);
+    res.json({ message: 'Invalid username or password' });
+    return;
+  }
+  
+  const { title, content } = req.body;
+  const newNote = new Note({ userID: user._id, title, content});
+  
+  newNote
+    .save()
+    .then((note) => {
+      res.status(201).json(note);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Failed to create note' });
+    });
+});
+
+
+app.get('/notes', async (req, res) => {
+  const { authorization } = req.headers;
+  const [, token] = authorization.split(' ');
+  const [username, password] = token.split(':');
+  const user = await User.findOne({ username }).exec();
+  if (!user || user.password !== password) {
+    res.status(403);
+    res.json({ message: 'Invalid username or password' });
+    return;
+  }
+  
+  Note.find({ userID: user._id })
+    .then((notes) => {
+      res.json(notes);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Failed to retrieve notes' });
+    });
+});
+
+
+app.delete('/notes/:id', (req, res) => {
+  const id = req.params.id;
+  Note.findByIdAndRemove(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.error('Error deleting note:', err);
+      res.sendStatus(500);
+    });
+});
+ 
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({username}).exec();
+  if(user){
+    res.status(500);
+    res.json({message: 'User already exists'});
+  }
+  const newUser = new User({username, password });
+  await newUser
+    .save()
+    .then((user) => {
+      res.status(201).json({message: 'User created'});
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Failed to create user' });
+    });
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username }).exec();
+  if (!user || user.password !== password) {
+    res.status(403);
+    res.json({ message: 'Invalid username or password' });
+  } else {
+    res.json({ message: 'Login successful' });
+  }
+});
